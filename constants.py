404cec3868c092889cee874e29e3b99a5a6670c1
Thea Holtlund Jacobsen <thj1@st-andrews.ac.uk>
2023-06-20 19:49:36 +0100

    Create separate file for variables that are constant

---
+++ constants.py
@@ -0,0 +1,7 @@
+# Defining constants for Data Analytics Framework for GAP Package Management
+
+# Import modules from other project scripts
+from config import *
+
+# Constants for organisation
+ORG_NAME = "gap-packages"
