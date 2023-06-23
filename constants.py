09e192f5e22fc7921da2963ad9c4e1c51f2eb3e5
Thea Holtlund Jacobsen <thj1@st-andrews.ac.uk>
2023-06-23 14:44:59 +0100

    Move out constants

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 # Defining constants for Data Analytics Framework for GAP Package Management
 
+# Import required libraries and packages
+from github import Github
+
 # Import modules from other project scripts
 from config import *
 
