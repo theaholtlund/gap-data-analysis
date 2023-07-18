eff761c61aebd9d960110ed133b6ee4838a4b1ee
Thea Holtlund Jacobsen <thj1@st-andrews.ac.uk>
2023-07-18 19:37:12 +0100

    Add Docstrings and function annotations to all functions

--- setup/utils.py
+++ setup/utils.py
@@ -5,9 +5,24 @@ import os
 from pathlib import Path
 from github import Github
 
-# Define function to authorise a user with their GitHub token
-# Code inspired by fingolfin, gap-system: https://github.com/gap-system/gap/blob/master/dev/releases/utils.py
-def get_github_token():
+
+def get_github_token() -> str:
+    """Get the GitHub token for user authorization.
+
+    Args:
+        None
+
+    Returns:
+        str: The GitHub token.
+
+    Raises:
+        FileNotFoundError: If the GitHub token file does not exist.
+        Exception: If there is an error reading the GitHub token file.
+
+    Notes:
+        This code was inspired by fingolfin's implementation in the gap-system repository:
+        https://github.com/gap-system/gap/blob/master/dev/releases/utils.py
+    """
     token_file_path = os.path.expanduser('~/.github_token')
 
     try:
