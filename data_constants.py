cbc09c609e71cd3ef59de5c89e1d7aea103c1c52
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2023-08-13 13:59:04 +0100

    Move sleep function from data constants to utils file

--- data_constants.py
+++ data_constants.py
@@ -2,8 +2,6 @@
 
 # Import modules and modules from other project scripts
 from setup.config import *
-import time
-from datetime import datetime
 
 # Constants for organisation
 ORG_NAME_PACKAGES = "gap-packages"
@@ -11,28 +9,3 @@ ORG_NAME_SYSTEM = "gap-system"
 
 # Constants for repositories
 DISTRO_REPO = "PackageDistro"
-
-def wait_until_reset(reset_time: int) -> None:
-    """Wait until the GitHub API rate limit resets by calculating the time difference between
-    the current time and the provided reset time. If the reset time is in the future, it sleeps for the time difference.
-
-    Args:
-        reset_time (int): The reset time of the GitHub API rate limit in UNIX timestamp format.
-
-    Returns:
-        None        
-    """
-
-    # Convert UNIX timestamp to a datetime object
-    reset_datetime = datetime.fromtimestamp(reset_time)
-
-    # Calculate the time difference between current time and reset time
-    current_time = datetime.now()
-    time_difference = reset_datetime - current_time
-
-    # Sleep only if the reset time is in the future
-    if time_difference.total_seconds() > 0:
-        sleep_time = time_difference.total_seconds()
-        sleep_time_minutes = sleep_time / 60
-        print(f"Reached GitHub API rate limit. Sleeping for {sleep_time_minutes} minutes until the limit resets.")
-        time.sleep(sleep_time)
--- setup/utils.py
+++ setup/utils.py
@@ -2,10 +2,12 @@
 
 # Import required libraries and packages
 import os
+import time
 from pathlib import Path
 from github import Github
+from datetime import datetime
 
-
+# Get the GitHub token and set it for the environment
 def get_github_token() -> str:
     """Get the GitHub token for user authorisation.
 
@@ -38,3 +40,29 @@ def get_github_token() -> str:
         print(f"GitHub token file does not exist in '{token_file_path}'.")
     except Exception as exception:
         print(f"Error reading GitHub token file: {str(exception)}")
+
+# Sleep function to pause data extraction process if API calls run out
+def wait_until_reset(reset_time: int) -> None:
+    """Wait until the GitHub API rate limit resets by calculating the time difference between
+    the current time and the provided reset time. If the reset time is in the future, it sleeps for the time difference.
+
+    Args:
+        reset_time (int): The reset time of the GitHub API rate limit in UNIX timestamp format.
+
+    Returns:
+        None        
+    """
+
+    # Convert UNIX timestamp to a datetime object
+    reset_datetime = datetime.fromtimestamp(reset_time)
+
+    # Calculate the time difference between current time and reset time
+    current_time = datetime.now()
+    time_difference = reset_datetime - current_time
+
+    # Sleep only if the reset time is in the future
+    if time_difference.total_seconds() > 0:
+        sleep_time = time_difference.total_seconds()
+        sleep_time_minutes = sleep_time / 60
+        print(f"Reached GitHub API rate limit. Sleeping for {sleep_time_minutes} minutes until the limit resets.")
+        time.sleep(sleep_time)
