96b87c81c01b34c56672e31470af175b002d87e3
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-14 16:25:20 +0100

    Add functionality to display analysis data on dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,38 @@
 # Import required modules and libraries
+import os
+import nbformat
 import streamlit as st
 
-# Import modules from other project scripts
-from data_constants import *
+# Function to display data analysis outputs
+def display_data_analysis_output():
+    notebook_path = "notebooks/05_data_analysis.ipynb"
+    output_html = []
+
+    with open(notebook_path, "r", encoding="utf-8") as f:
+        notebook_content = f.read()
+
+    notebook = nbformat.reads(notebook_content, as_version=4)
+
+    for cell in notebook.cells:
+        if cell.cell_type == "code" and cell.get("outputs", []):
+            for output in cell.outputs:
+                if output.output_type == "display_data":
+                    if "data" in output and "text/html" in output.data:
+                        output_html.append(output.data["text/html"])
+                elif output.output_type == "execute_result":
+                    if "data" in output and "text/plain" in output.data:
+                        output_text = output.data["text/plain"]
+                        output_html.append(f"<pre>{output_text}</pre>")
+                elif output.output_type == "stream":
+                    if "text" in output:
+                        output_text = output.text
+                        if "Request limit for API Calls:" not in output_text:
+                            output_html.append(f"<pre>{output_text}</pre>")
+
+    st.write("Data analysis outputs:")
+    for output in output_html:
+        st.markdown(output, unsafe_allow_html=True)
+
 
 # Set up Streamlit dashboard with page navigation
 def main():
@@ -19,10 +49,13 @@ def main():
     # Repositories content
     elif page == "Repositories":
         st.header("Repositories Page")
+        display_data_analysis_output()
 
     # Community content
     elif page == "Community":
         st.header("Community Page")
 
+# Check if script is being run directly, as the main program
+# If so, call the main() function to set up and run Streamlit dashboard
 if __name__ == "__main__":
     main()
