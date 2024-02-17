4510df91ed8deb225452811f2cbff40317a66fac
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-17 18:07:12 +0100

    Add functionality to allow dynamic notebook upload

--- dashboard.py
+++ dashboard.py
@@ -33,16 +33,14 @@ def process_notebook_content(notebook_path):
     return outputs
 
 # Function to display data analysis outputs
-def display_data_analysis_output():
-    output_html = process_notebook_content("notebooks/05_data_analysis.ipynb")
+def display_data_analysis_output(notebook_path):
+    output_html = process_notebook_content(notebook_path)
 
     for output in output_html:
         st.markdown(output, unsafe_allow_html=True)
 
 # Function to display data visualisation outputs
-def display_data_visualisation_output():
-    notebook_path = "notebooks/06_data_visualisation.ipynb"
-
+def display_data_visualisation_output(notebook_path):
     outputs = process_notebook_content(notebook_path)
 
     for output in outputs:
@@ -55,15 +53,31 @@ def main():
     # Sidebar options
     page = st.sidebar.selectbox("Select a page", ["Data Analysis", "Data Visualisation"])
 
-    # Data analysis content
+    # If data Analysis selected
     if page == "Data Analysis":
         st.header("Data Analysis Outputs")
-        display_data_analysis_output()
+        
+        # Allow user to upload a notebook
+        uploaded_file = st.file_uploader("Upload a notebook", type=["ipynb"])
+        if uploaded_file is not None:
+            # Process and display uploaded notebook
+            display_data_analysis_output(uploaded_file)
+        else:
+            # Display default notebook
+            display_data_analysis_output("notebooks/05_data_analysis.ipynb")
 
-    # Data visualisation content
+    # If data Visualisation selected
     elif page == "Data Visualisation":
         st.header("Data Visualisation Outputs")
-        display_data_visualisation_output()
+        
+        # Allow user to upload a notebook
+        uploaded_file = st.file_uploader("Upload a notebook", type=["ipynb"])
+        if uploaded_file is not None:
+            # Process and display uploaded notebook
+            display_data_visualisation_output(uploaded_file)
+        else:
+            # Display default notebook
+            display_data_visualisation_output("notebooks/06_data_visualisation.ipynb")
 
 # Check if script is being run directly, as the main program
 # If so, call the main() function to set up and run Streamlit dashboard
